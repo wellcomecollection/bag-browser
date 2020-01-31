c71971ef3e5a8344e9b2b6b6783a2e92cef715b3
Alex Chan <a.chan@wellcome.ac.uk>
2020-01-31 14:32:27 +0000

    Remove a bit more unused code

--- get_bag_info.py
+++ get_bag_info.py
@@ -1,62 +1,9 @@
 #!/usr/bin/env python
 
-import itertools
-import sqlite3
-
-from src.database import BagsDatabase, SqliteDatabase
+from src.database import BagsDatabase
 from src.storage_service import StorageService
 
 import tqdm
-
-
-def create_table(cursor):
-    try:
-        cursor.execute(
-            """CREATE TABLE bags
-            (
-                id TEXT PRIMARY KEY,
-                space TEXT,
-                external_identifier TEXT,
-                version INTEGER,
-                created_date TEXT,
-                file_count INTEGER,
-                total_file_size INTEGER
-            )"""
-        )
-    except sqlite3.OperationalError as err:
-        if str(err) == "table bags already exists":
-            pass
-        else:
-            raise
-
-    try:
-        cursor.execute(
-            """CREATE TABLE file_extensions (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                bag_id TEXT,
-                extension TEXT,
-                count INTEGER,
-                CONSTRAINT fk_storage_key
-                    FOREIGN KEY (bag_id)
-                    REFERENCES bags(id),
-                UNIQUE (bag_id, extension)
-            )"""
-        )
-    except sqlite3.OperationalError as err:
-        if str(err) == "table file_extensions already exists":
-            pass
-        else:
-            raise
-
-
-def chunked_iterable(iterable, size):
-    # https://alexwlchan.net/2018/12/iterating-in-fixed-size-chunks/
-    it = iter(iterable)
-    while True:
-        chunk = tuple(itertools.islice(it, size))
-        if not chunk:
-            break
-        yield chunk
 
 
 if __name__ == "__main__":
