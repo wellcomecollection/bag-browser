77a5c6704ef4a5d3a2b7db4bf5b74ee034667ace
Alex Chan <a.chan@wellcome.ac.uk>
2020-01-31 13:30:12 +0000

    Add some code for interacting with the storage service

---
+++ src/storage_service.py
@@ -0,0 +1,54 @@
+import json
+from typing import Iterable
+
+import attr
+import boto3
+
+from models import Bag, BagIdentifier
+
+
+@attr.s
+class StorageService:
+    table_name = attr.ib()
+
+    def get_bag_identifiers(self) -> Iterable[BagIdentifier]:
+        dynamodb = boto3.resource("dynamodb").meta.client
+
+        paginator = dynamodb.get_paginator("scan")
+
+        for page in paginator.paginate(TableName=self.table_name):
+            for item in page["Items"]:
+
+                # The ID is of the form
+                #
+                #   {space}/{external_identifier}
+                #
+                space, external_identifier = item["id"].split("/")
+                version = int(item["version"])
+
+                yield BagIdentifier(
+                    space=space,
+                    external_identifier=external_identifier,
+                    version=version,
+                )
+
+    def get_bag(self, bag_identifier: BagIdentifier) -> Bag:
+        dynamodb = boto3.resource("dynamodb").meta.client
+        s3 = boto3.client("s3")
+
+        ddb_key = {
+            "id": "/".join([bag_identifier.space, bag_identifier.external_identifier]),
+            "version": bag_identifier.version,
+        }
+
+        ddb_resp = dynamodb.get_item(TableName=self.table_name, Key=ddb_key)
+
+        item = ddb_resp["Item"]
+
+        s3_bucket = item["payload"]["typedStoreId"]["namespace"]
+        s3_key = item["payload"]["typedStoreId"]["path"]
+
+        s3_body = s3.get_object(Bucket=s3_bucket, Key=s3_key)["Body"]
+        storage_manifest = json.load(s3_body)
+
+        return Bag.from_storage_manifest(storage_manifest)
---
+++ tests/test_storage_service.py
@@ -0,0 +1,117 @@
+import contextlib
+import secrets
+
+import boto3
+from moto import mock_dynamodb2, mock_s3
+
+from models import Bag, BagIdentifier
+from storage_service import StorageService
+
+
+@contextlib.contextmanager
+def manifests_table():
+    table_name = f"table-{secrets.token_hex()}"
+
+    dynamodb = boto3.client("dynamodb")
+
+    dynamodb.create_table(
+        AttributeDefinitions=[
+            {"AttributeName": "id", "AttributeType": "S"},
+            {"AttributeName": "version", "AttributeType": "N"},
+        ],
+        TableName=table_name,
+        KeySchema=[
+            {"AttributeName": "id", "KeyType": "HASH"},
+            {"AttributeName": "version", "KeyType": "RANGE"},
+        ],
+    )
+
+    yield table_name
+
+    dynamodb.delete_table(TableName=table_name)
+
+
+@contextlib.contextmanager
+def s3_bucket():
+    s3 = boto3.client("s3")
+
+    bucket_name = f"s3-{secrets.token_hex(5)}"
+    s3.create_bucket(Bucket=bucket_name)
+    yield bucket_name
+
+    # We can't delete a bucket unless it's empty, so we have to delete all
+    # the objects first.
+    paginator = s3.get_paginator("list_objects_v2")
+    for page in paginator.paginate(Bucket=bucket_name):
+        for s3_obj in page["Contents"]:
+            s3.delete_object(Bucket=bucket_name, Key=s3_obj["Key"])
+
+    s3.delete_bucket(Bucket=bucket_name)
+
+
+@mock_dynamodb2
+def test_can_read_single_bag_id():
+    dynamodb = boto3.resource("dynamodb")
+
+    with manifests_table() as table_name:
+        table = dynamodb.Table(table_name)
+        table.put_item(Item={"id": "example_space/example_identifier", "version": 2})
+
+        ss = StorageService(table_name=table_name)
+        result = list(ss.get_bag_identifiers())
+
+    assert result == [
+        BagIdentifier(
+            space="example_space", external_identifier="example_identifier", version=2
+        )
+    ]
+
+
+@mock_dynamodb2
+def test_can_read_lots_of_bag_ids():
+    dynamodb = boto3.resource("dynamodb")
+
+    with manifests_table() as table_name:
+        table = dynamodb.Table(table_name)
+
+        for version in range(1250):
+            table.put_item(
+                Item={"id": "example_space/example_identifier", "version": version}
+            )
+
+        ss = StorageService(table_name=table_name)
+        assert len(list(ss.get_bag_identifiers())) == 1250
+
+
+@mock_dynamodb2
+@mock_s3
+def test_can_get_storage_manifest():
+    dynamodb = boto3.resource("dynamodb")
+    s3 = boto3.client("s3")
+
+    with manifests_table() as table_name, s3_bucket() as bucket_name:
+        table = dynamodb.Table(table_name)
+
+        s3.upload_file(
+            Bucket=bucket_name,
+            Key="b10109377.json",
+            Filename="tests/manifests/b10109377.json",
+        )
+
+        table.put_item(
+            Item={
+                "id": "digitised/b10109377",
+                "version": 1,
+                "payload": {
+                    "typedStoreId": {"namespace": bucket_name, "path": "b10109377.json"}
+                },
+            }
+        )
+
+        ss = StorageService(table_name=table_name)
+        bag_identifier = next(ss.get_bag_identifiers())
+
+        bag = ss.get_bag(bag_identifier=bag_identifier)
+
+        assert isinstance(bag, Bag)
+        assert bag.id == "digitised/b10109377/v1"
